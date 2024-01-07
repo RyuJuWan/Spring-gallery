b21597e128fd510b3e3af03bd8158005a26c0ada
RyuJuWan <gogoxotj@naver.com>
2024-01-07 23:23:21 +0900

    add order

---
+++ src/main/java/org/africalib/galley/backend/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package org.africalib.galley.backend.repository;
+
+import org.africalib.galley.backend.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+
+
+}
