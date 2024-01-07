48aeea3b7dc40540a089df602c511ec150a357df
RyuJuWan <gogoxotj@naver.com>
2024-01-07 22:34:34 +0900

    add cart

---
+++ src/main/java/org/africalib/galley/backend/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package org.africalib.galley.backend.repository;
+
+import org.africalib.galley.backend.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    List<Cart> findByMemberId(int memberId);
+
+    Cart findByMemberIdAndItemId(int memberId, int itemId);
+
+
+}
--- src/main/java/org/africalib/galley/backend/service/JwtService.java
+++ src/main/java/org/africalib/galley/backend/service/JwtService.java
@@ -5,4 +5,8 @@ import io.jsonwebtoken.Claims;
 public interface JwtService {
     String getToken(String key, Object value);
     Claims getClaims(String token);
+
+    Boolean isValid(String token);
+
+    int getId(String token);
 }
